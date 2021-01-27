4e20d4035ec2cd400409f703eb73ead0f90a7838
hschne <hans.schnedlitz@gmail.com>
2021-01-27 20:35:36 +0100

    Add customized DB

--- lib/schienenzeppelin/app_generator.rb
+++ lib/schienenzeppelin/app_generator.rb
@@ -3,10 +3,24 @@
 module Schienenzeppelin
   class AppGenerator < Rails::Generators::AppGenerator
 
+    class_option :database,
+                 type: :string,
+                 aliases: '-d',
+                 default: 'postgresql',
+                 desc: "Preconfigure for selected database (options: #{DATABASES.join('/')})"
+
     def create_root_files
       super
 
       build(:irbrc)
+    end
+
+    def database_yml
+      if options[:database] == 'postgresql'
+        template 'config/postgresql.yml.erb', 'config/database.yml'
+      else
+        super
+      end
     end
 
     protected
