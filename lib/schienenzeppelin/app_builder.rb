091dc87f352c809b89dda2a51c053cb064181f7e
hschne <hans.schnedlitz@gmail.com>
2021-01-27 20:18:17 +0100

    More customization

--- lib/schienenzeppelin/app_builder.rb
+++ lib/schienenzeppelin/app_builder.rb
@@ -9,5 +9,13 @@ module Schienenzeppelin
     def ruby_version
       template '.tool-versions.erb', '.tool-versions'
     end
+
+    def gitignore
+      template '.gitignore.erb', '.gitignore'
+    end
+
+    def irbrc
+      template '.irbrc.erb', '.irbrc'
+    end
   end
 end
--- lib/schienenzeppelin/app_generator.rb
+++ lib/schienenzeppelin/app_generator.rb
@@ -2,5 +2,17 @@
 
 module Schienenzeppelin
   class AppGenerator < Rails::Generators::AppGenerator
+
+    def create_root_files
+      super
+
+      build(:irbrc)
+    end
+
+    protected
+
+    def get_builder_class
+      Schienenzeppelin::AppBuilder
+    end
   end
 end
